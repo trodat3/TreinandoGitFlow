3751659aa08acc4483d4ea707ce2eac6566fa6bd
trodat3 <joaopedroabrantescarvalho@gmail.com>
2025-10-21 11:19:39 -0300

    Atualizando padrão do roteamento colocando os nomes. Criei minha página de perfil

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
+import Levi from "./pages/Levi";
+import JoaoPedro from "./pages/JoaoPedro";
 
 export default function App() {
   return (
@@ -8,6 +10,8 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/levi" element={<Levi />} />
+        <Route path="/joaopedro" element={<JoaoPedro />} />
       </Routes>
     </Router>
   );
